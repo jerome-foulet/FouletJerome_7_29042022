5de07dca1411cad73c8376ab1108c9e5f33eb7c6
Jérôme FOULET <Gom3.78@Gmail.Com>
2022-05-19 15:25:58 +0200

    feat(search): implement native search

--- scripts/sentenceSearch.js
+++ scripts/sentenceSearch.js
@@ -1,4 +1,21 @@
 const searchSentenceInRecipesArray = (inputToSearch, recipesArray) => {
-  return recipesArray
+  let matchedRecipes = []
+  for (let recipesIndex = 0; recipesIndex < recipesArray.length; recipesIndex++) {
+    const recipe = recipesArray[recipesIndex]
+    // Create string of ingredients
+    let ingredients = ''
+    for (let ingredientIndex = 0; ingredientIndex < recipe.ingredients.length; ingredientIndex++) {
+      const ingredient = recipe.ingredients[ingredientIndex]
+      // Space to avoid fusion of 2 ingredients
+      ingredients += ` ${ingredient.ingredient}`
+    }
+    // Space to avoid fusion of our 3 use case
+    const sentenceToSearchIn = `${recipe.name} ${recipe.description} ${ingredients}`
+    // Search input into sentence
+    if (sentenceToSearchIn.toLowerCase().indexOf(inputToSearch.toLowerCase(), 0) !== -1) {
+      matchedRecipes.push(recipe)
+    }
+  }
+  return matchedRecipes
 }
 export { searchSentenceInRecipesArray };
