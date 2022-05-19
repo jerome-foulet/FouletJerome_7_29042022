6fa199e6cbd47ba08cb070f840677df5b197933c
Jérôme FOULET <Gom3.78@Gmail.Com>
2022-05-19 15:35:04 +0200

    feat(search): implement functional search

--- scripts/sentenceSearch.js
+++ scripts/sentenceSearch.js
@@ -1,4 +1,14 @@
 const searchSentenceInRecipesArray = (inputToSearch, recipesArray) => {
-  return recipesArray
+  // Filter recipes
+  return recipesArray.filter(recipe => {
+    // Create string of ingredients
+    const ingredients = recipe.ingredients
+      .map(ingredient => ingredient.ingredient)
+      .join(' ');
+      // Search input into sentence and return true or false to filter current recipe
+    return `${recipe.name} ${recipe.description} ${ingredients}`
+      .toLocaleLowerCase()
+      .includes(inputToSearch.toLowerCase());
+  });
 }
 export { searchSentenceInRecipesArray };
