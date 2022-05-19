f6c58dc46ae3fbf94c90ba9c85d15bed57e7a255
Jérôme FOULET <Gom3.78@Gmail.Com>
2022-05-19 15:20:36 +0200

    feat(search): principal search not implemented

--- scripts/sentenceSearch.js
+++ scripts/sentenceSearch.js
@@ -1,12 +1,4 @@
 const searchSentenceInRecipesArray = (inputToSearch, recipesArray) => {
-  return recipesArray.filter(recipe => {
-    const ingredients = recipe.ingredients
-      .map(ingredient => ingredient.ingredient.toLowerCase())
-      .join(' ')
-    return [recipe.name, recipe.description, ingredients]
-      .join(' ')
-      .toLocaleLowerCase()
-      .includes(inputToSearch.toLowerCase())
-  })
+  return recipesArray
 }
 export { searchSentenceInRecipesArray };
